3cafb56454ccd066cf457e8273d02f198d575484
OblateSpheroid <celica6@juno.com>
2017-05-16 10:36:17 -0400

    Add files via upload

---
+++ install.r
@@ -0,0 +1,13 @@
+if(!interactive())(file <- commandArgs(T)[1]) else (
+  file <- readline("Enter text file of packages: "))
+needed.pkgs <- as.character(as.matrix(read.table(file)))
+repo <- "http://cran.us.r-project.org"
+
+install.needed <- function(){
+  current.pkgs<-as.character(as.data.frame(installed.packages())$Package)
+  to_add <- needed.pkgs[which(!needed.pkgs %in% current.pkgs)]
+  for (i in 1:length(to_add)) install.packages(to_add[i], repos=repo)
+}
+
+#if(!interactive()) install.needed()
+if(!interactive())(print(paste0("Will install libraries in ",file))) else cat("Run install.needed() to install\n")
